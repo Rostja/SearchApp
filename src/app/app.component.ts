11463185afe2d0ae68b8c8dc37b645005a0be228
Rostja <rasto.dorcak@gmail.com>
2024-09-24 21:51:23 +0200

    Changes to be committed: #	modified:   src/app/app-routing.module.ts #	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
+  imports: [RouterModule],
   template: `
-    <h1>Welcome to {{title}}🦋!</h1>
+    <!--header-->
+    <div class="navbar">
+      <!--logo-->
+      <div class="navbar-brand">
+        <a class="navbar-item"> </a>
+      </div>
+    </div>
 
-    
+    <router-outlet></router-outlet>
+
+
+    <!--footer-->
+    <footer>
+      <p>Copyright &copy; {{ title }}. All rights reserved.</p>
+    </footer>
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'SearchApp';
+  title = "SearchApp";
 }
